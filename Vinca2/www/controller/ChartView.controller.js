a51bc4470d36371429e8bf3095e326cdd824dc50
mandapaj <jangomandapat@gmail.com>
2018-08-01 23:11:02 +0800

    Menu button made to work
    Menu button made to work

--- Vinca2/www/controller/ChartView.controller.js
+++ Vinca2/www/controller/ChartView.controller.js
@@ -24,6 +24,50 @@ sap.ui.define([
 			
 		},
 
+		getRouter: function() {
+			return this.getOwnerComponent().getRouter();
+		},
+
+
+		fnNavigateToMaster : function(){
+			this.getRouter().navTo("masterdata");
+		},
+
+		handleMenuItemPress: function(oEvent) {
+			if (oEvent.getParameter("item").getSubmenu()) {
+				return;
+			}
+
+			var msg = "";
+			if (oEvent.getParameter("item").getMetadata().getName() == "sap.ui.unified.MenuTextFieldItem") {
+				msg =  oEvent.getParameter("item").getValue();
+			} else {
+				msg = oEvent.getParameter("item").getText();
+				if(msg===">Dein Stromverbrauch"){
+					this.fnNavigateToChart();
+				}else if (msg===">Stammdaten"){
+					this.fnNavigateToMaster();
+				}
+			}
+
+			//MessageToast.show(msg);
+		},
+
+		handlePressOpenMenu: function(oEvent) {
+			var oButton = oEvent.getSource();
+
+			// create menu only once
+			if (!this._menu) {
+				this._menu = sap.ui.xmlfragment(
+					"Vinca.view.fragment.menu",
+					this
+				);
+				this.getView().addDependent(this._menu);
+			}
+
+			var eDock = sap.ui.core.Popup.Dock;
+			this._menu.open(this._bKeyboard, oButton, eDock.BeginTop, eDock.BeginBottom, oButton);
+		},
 
 
 
