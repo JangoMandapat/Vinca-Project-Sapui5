3ceb6c933c410e62d0643cf96a64ac9d882a6bbe
mandapaj <jangomandapat@gmail.com>
2018-08-09 20:40:54 +0800

    masterdata functionality
    masterdata functionality

--- Vinca2/www/controller/MasterData.controller.js
+++ Vinca2/www/controller/MasterData.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
 	
-], function(Controller) {
+], function(Controller, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("Vinca.controller.MasterData", {
@@ -53,6 +55,64 @@ sap.ui.define([
 
 			var eDock = sap.ui.core.Popup.Dock;
 			this._menu.open(this._bKeyboard, oButton, eDock.BeginTop, eDock.BeginBottom, oButton);
+		},
+
+		OnSave : function () {
+			//insert username in login page
+				
+				// var oUser = this.getView().getModel().getProperty("/username");
+				// var oPassword = this.getView().getModel().getProperty("/password");
+				var oView = this.getView();
+				var that = this;
+				var id = 1;
+				var reduction = this.getView().byId("Stromabschlag").getValue();
+				var cost = this.getView().byId("Stromkosten").getValue();
+				var fee = this.getView().byId("Grundgebuhren").getValue();
+				var oHtml;
+				var VincaMasterData = new JSONModel();
+				var sHost = "https://pipemonplus-q.open-grid-europe.com/oge/apps/vinca/GetData/";
+				var lUrl = "VincaMasterData.xsjs?id=" + id +"&reduction=" + reduction + "&cost=" + cost + "&fee=" + fee;
+
+				oView.setModel(VincaMasterData, "VincaMasterData"); 
+			$.ajax({
+                        url: sHost+lUrl,
+                        type: "GET",
+                        async: false,
+                        success: function(data, textStatus, XMLHttpRequest) {
+                            console.log(XMLHttpRequest);
+  							VincaMasterData.setData(data);
+  							
+                            oView.setModel(VincaMasterData, "VincaMasterData"); 
+                            //oView.byId("Stromkosten").setModel(VincaCostDataModel);
+                            /*oView.createContent("VincaTestDataModel");*/                  
+
+                        },
+                        error: function(XMLHttpRequest, textStatus, errorThrown) {
+                            MessageToast.show("Error : " + textStatus);
+                            console.log(XMLHttpRequest);
+                            console.log(errorThrown);
+                            //                             that.fnDisplayAjaxMessage(XMLHttpRequest); 
+                           
+                        },
+                        timeout: 12000 //timeout to 12sec
+                    });
+		
+			//var iStatus = this.getView().getModel("VincaMasterData").getProperty("/rs0/0/STATUS");
+			var sMessage = this.getView().getModel("VincaMasterData").getProperty("/rs0/0/MSG");
+			//if (iStatus === 1){
+					
+				//this.getRouter().navTo("App");
+            //	MessageToast.show("user name and password correct");
+            //	this.fnNavigateToHome();
+
+        	//}
+        	
+          
+         // else {
+          MessageToast.show(sMessage);
+         // }
+				
+          
 		}
 	});
 });
